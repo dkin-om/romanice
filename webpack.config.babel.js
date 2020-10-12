db607313bbabafe4ef46ea0b6a5e69993b3193c4
dkin-om <67123388+dkin-om@users.noreply.github.com>
2020-10-12 20:30:46 +0530

    Webpack umd bundle

---
+++ webpack.config.babel.js
@@ -0,0 +1,22 @@
+import { resolve as resolvePath } from 'path';
+
+export default {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: resolvePath(__dirname, 'dist'),
+    filename: 'romanice.min.js',
+    library: 'romanice',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        include: resolvePath(__dirname, 'src'),
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
